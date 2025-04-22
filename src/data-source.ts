70dd4dab2db1bffde25a5aae2f344eceed857260
GregPinheiro <>
2025-04-22 09:14:09 -0300

    Feature: Add migration

---
+++ src/data-source.ts
@@ -0,0 +1,16 @@
+import { config } from 'dotenv';
+config();
+
+import { DataSource } from 'typeorm';
+import { User } from './commom/database/entities';
+
+export default new DataSource({
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '3306'),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [User],
+  migrations: ['src/migrations/*.ts'],
+});
